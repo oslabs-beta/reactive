a1881ace6026a57d3a712dbab3ed194d8cf2cfb8
Colin Rooney <102275700+12mv2@users.noreply.github.com>
2024-10-06 20:00:01 -0600

    cleaned up webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,40 +1,3 @@
-// const path = require('path');
-// const HtmlWebPackPlugin = require('html-webpack-plugin');
-// const CopyPlugin = require('copy-webpack-plugin');
-
-// module.exports = {
-//   mode: 'development',
-//   entry: './extension.js',
-//   output: {
-//     path: path.resolve(__dirname, 'dist'),
-//     filename: 'bundle.js',
-//   },
-//   module: {
-//     rules: [
-//       {
-//         test: /\.(js|jsx)$/,
-//         exclude: /node_modules/,
-//         use: ['babel-loader'],
-//         options: {
-//             presets: ['@babel/env', '@babel/react'],
-//           },
-//       },
-//     ],
-//     resolve: {
-//         extensions: ['.js', '.jsx'],
-//       },
-//   },
-// //   plugins: [
-// //     new HtmlWebPackPlugin({
-// //       template: './src/index.html',
-// //       filename: './index.html',
-// //     }),
-//     // new CopyPlugin({
-//     //   patterns: [{ from: './src/style.css' }],
-//     // }),
-// //   ]
-// };
-
 const path = require('path');
 
 module.exports = {
@@ -69,13 +32,4 @@ module.exports = {
   resolve: {
     extensions: ['.js', '.jsx'],
   },
-  // plugins: [
-  //   new HtmlWebPackPlugin({
-  //     template: './src/index.html',
-  //     filename: './index.html',
-  //   }),
-  //   // new CopyPlugin({
-  //   //   patterns: [{ from: './src/style.css' }],
-  //   // }),
-  // ]
 };
