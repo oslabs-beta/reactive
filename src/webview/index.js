43e5d95c440ed4a66f7d0ffe96f2ea61f23a0578
Colin Rooney <102275700+12mv2@users.noreply.github.com>
2024-10-17 23:04:02 -0600

    feat: dendrogram component with dummy data and d3 rendering, d3 added to package,json, webview HTML updated in extension.js to include styles, webpack.config updated to handle JSX

--- src/webview/index.js
+++ src/webview/index.js
@@ -1,3 +1,20 @@
+// 
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Dendrogram from './Dendrogram';
+
+const App = () => {
+  return (
+    <div>
+      <h1>React Component Tree</h1>
+      <Dendrogram />
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
 // import React from 'react';
 // import { createRoot } from 'react-dom/client';
 // import App from './App';
