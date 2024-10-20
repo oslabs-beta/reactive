3ded83a7fff50210ace6881ee7e580654178440c
Colin Rooney <102275700+12mv2@users.noreply.github.com>
2024-10-20 13:23:59 -0600

    ts and tsx added to webpack to test for

--- webpack.config.js
+++ webpack.config.js
@@ -20,7 +20,7 @@ module.exports = [
     module: {
       rules: [
         {
-          test: /\.(js|jsx)$/,
+          test: /\.(ts|tsx|js|jsx)$/,
           exclude: /node_modules/,
           use: {
             loader: 'babel-loader',
