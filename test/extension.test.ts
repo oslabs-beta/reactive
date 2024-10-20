e6e3cda1edeb45e9add56c4af150de1484dedd99
Colin Rooney <102275700+12mv2@users.noreply.github.com>
2024-10-20 00:58:57 -0600

    fix/extension.test.ts converted to ts

--- test/extension.test.ts
+++ test/extension.test.ts
@@ -1,15 +1,13 @@
-const assert = require('assert');
-
-// You can import and use all API from the 'vscode' module
-// as well as import your extension to test it
-const vscode = require('vscode');
-// const myExtension = require('../extension');
+import * as assert from 'assert';
+import * as vscode from 'vscode';
 
 suite('Extension Test Suite', () => {
-	vscode.window.showInformationMessage('Start all tests.');
+  suiteTeardown(() => {
+    vscode.window.showInformationMessage('All tests done!');
+  });
 
-	test('Sample test', () => {
-		assert.strictEqual(-1, [1, 2, 3].indexOf(5));
-		assert.strictEqual(-1, [1, 2, 3].indexOf(0));
-	});
+  test('Sample test', () => {
+    assert.strictEqual(-1, [1, 2, 3].indexOf(5));
+    assert.strictEqual(-1, [1, 2, 3].indexOf(0));
+  });
 });
