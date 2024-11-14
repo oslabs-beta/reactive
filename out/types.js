0ae1f9904515d7d7349696407517c47065f81d7a
Colin Rooney <102275700+12mv2@users.noreply.github.com>
2024-11-14 15:34:10 -0700

    update: vscodegitignore built out, files removed from package.json

--- out/types.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-// The types.ts file is used to define reusable types and interfaces in a TypeScript project. It serves as a central place to store type definitions that can be imported and used throughout your project to enforce type safety and improve code organization. Here's what these types might be used for:
-// disposable, filter, tree
-Object.defineProperty(exports, "__esModule", { value: true });
-//REATIVE/src/types.ts
-// TreeObject: This type could be used to represent a hierarchical file structure, such as a folder tree or a component tree. Each TreeObject node contains a file and optional children, enabling recursive nesting to represent directory structures or nested components.
-//# sourceMappingURL=types.js.map
